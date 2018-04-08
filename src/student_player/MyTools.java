2b61326aa984717d4aac860cee8f8028ca1716cc
Ringgold <ringgoldlin@yahoo.com>
2018-04-07 23:58:00 -0400

    update the MyTool.java as the main function storing java file

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,143 @@
 package student_player;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+import boardgame.Move;
+import coordinates.Coord;
+import coordinates.Coordinates;
+import tablut.TablutBoardState;
+import tablut.TablutMove;
+
 public class MyTools {
+	//Weights for calculation
+	private static int Muscov_UserPieceWeight = 18;
+	private static int Muscov_OppoPieceWeight = 30;
+	private static int Muscov_SurroundWeight = 20;
+	private static int Swede_UserPieceWeight = 30;
+	private static int Swede_OppoPieceWeight = 18;
+	private static int Swede_SurroundWeight = 20;
+	private static int KingEscapingWeight = 45;
+	
     public static double getSomething() {
         return Math.random();
+    }
+    public static Move getWinnerMove(List<Node> children, int userID){
+    	for (Node child: children) {
+    		TablutBoardState state = child.getState();
+    		if (state.getWinner() == userID) {
+    			return child.getMove();
+    		}
+    	}
+    	return null;
+    }
+    
+    public static Move getEscapeMove(Node parent, int userID){
+    	Move bestMove = null;
+    	Coord king = parent.getState().getKingPosition();
+    	int minDistance = Coordinates.distanceToClosestCorner(king);
+        for (TablutMove move : parent.getState().getLegalMovesForPosition(king)) {
+            int moveDistance = Coordinates.distanceToClosestCorner(move.getEndPosition());
+            if (moveDistance < minDistance) {
+            	TablutBoardState childState = (TablutBoardState) parent.getState().clone();
+                childState.processMove(move);
+            	if (captureCheck(childState, userID)) {
+            		minDistance = moveDistance;
+                    bestMove = move;
+            	}
+            }
+        }
+        return bestMove;
+    }
+    
+    public static boolean captureCheck(TablutBoardState state, int userID){
+    	int originalNumPieces = state.getNumberPlayerPieces(userID);
+    	for (TablutMove move: state.getAllLegalMoves()) {
+    		TablutBoardState childState = (TablutBoardState) state.clone();
+            childState.processMove(move);
+    		int newNumPieces = childState.getNumberPlayerPieces(userID);
+    		if (originalNumPieces-newNumPieces != 0) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    public static Node getBestValue(List<Node> children){
+    	Node highestScore = children.get(0);
+    	for (Node child: children) {
+    		if (child.getValue() > highestScore.getValue()){
+    			highestScore = child;
+    		}
+    	}
+    	return highestScore;
+    }
+    
+    public static Node getGoodNode(Node start, int userID, int oppoID){
+    	start.buildChildren();
+    	int bestHeuristic = 0;
+    	Node goodNode = null;
+    	for (Node child: start.getChildren()){
+    		int heuristic = getHeuristicValue(child, userID, oppoID);
+    		if (heuristic > bestHeuristic){
+    			goodNode = child;
+    		}
+    	}
+    	return goodNode;
+    }
+    
+    public static int getHeuristicValue(Node child, int userID, int oppoID){
+    	int value = 2000;
+    	
+    	TablutBoardState currentState = child.getState();
+    	if (currentState.gameOver()){
+    		if (currentState.getWinner() == userID){
+    			return 99999;
+    		} 
+    		if (currentState.getWinner() == oppoID){
+    			return 0;
+    		}
+    	}
+    	
+    	Coord kingPosition = currentState.getKingPosition(); 
+    	HashSet<Coord> userPieces = currentState.getPlayerPieceCoordinates();
+    	HashSet<Coord> oppoPieces = currentState.getOpponentPieceCoordinates();
+    	
+    	// gain points for pieces being close to king, lose points for opponent being close to king
+    	
+    	if (userID == TablutBoardState.SWEDE) {
+    		for (Coord coord: currentState.getPlayerPieceCoordinates()) {
+        		value = value - Swede_SurroundWeight * coord.distance(kingPosition);
+    		}
+    		for (Coord coord: currentState.getOpponentPieceCoordinates()) {
+    			value = value + Swede_SurroundWeight * coord.distance(kingPosition);
+    		}
+    		value = value + Swede_UserPieceWeight * userPieces.size() -  Swede_OppoPieceWeight * oppoPieces.size();
+    		value -= KingEscapingWeight * Coordinates.distanceToClosestCorner(kingPosition);
+    	} else {
+    		for (Coord coord: currentState.getPlayerPieceCoordinates()) {
+        		value = value - Muscov_SurroundWeight * coord.distance(kingPosition);
+    		}
+    		for (Coord coord: currentState.getOpponentPieceCoordinates()) {
+    			value = value + Muscov_SurroundWeight * coord.distance(kingPosition);
+    		}
+    		value = value + Muscov_UserPieceWeight * userPieces.size() -  Muscov_OppoPieceWeight * oppoPieces.size();
+    		value += KingEscapingWeight * Coordinates.distanceToClosestCorner(kingPosition);
+    	}
+    	return value;
+    }
+    
+    public static TablutBoardState processState(TablutBoardState state){
+    	TablutBoardState tempState = (TablutBoardState) state.clone();
+        if (tempState.gameOver()){
+            return state;
+        }
+        while (!tempState.gameOver()){
+        	Random rand = new Random();
+        	List<TablutMove> moves = tempState.getAllLegalMoves();
+        	tempState.processMove(moves.get(rand.nextInt(moves.size())));
+        }
+        return tempState;
     }
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -14,16 +14,7 @@ public class StudentPlayer extends TablutPlayer {
 	private int user;
 	private int oppo;
 	
-	//Weights for calculation
-	private int Muscov_UserPieceWeight = 18;
-	private int Muscov_OppoPieceWeight = 30;
-	private int Muscov_SurroundWeight = 20;
-	private int Swede_UserPieceWeight = 30;
-	private int Swede_OppoPieceWeight = 18;
-	private int Swede_SurroundWeight = 20;
-	private int KingEscapingWeight = 45;
-	
-	private int TimeLimit = 1685;
+	private int TimeLimit = 1485;
 
     public StudentPlayer() {
         super("260561361");
@@ -43,14 +34,14 @@ public class StudentPlayer extends TablutPlayer {
     	start.buildChildren();
     	List<Node> children = start.getChildren(); 
     	
-    	Move winnerMove = getWinnerMove(children);
+    	Move winnerMove = MyTools.getWinnerMove(children, user);
     	if (winnerMove != null) {
     		return winnerMove;
     	}
     	
     	//try to espace when possible
     	if (user == TablutBoardState.SWEDE) {
-    		Move greedyMove = getEscapeMove(start);
+    		Move greedyMove = MyTools.getEscapeMove(start, user);
         	if (greedyMove != null) {
         		return greedyMove;
         	}
@@ -60,131 +51,13 @@ public class StudentPlayer extends TablutPlayer {
     	long startTime = System.currentTimeMillis();
     	while ( (System.currentTimeMillis()-startTime) < TimeLimit){
     		for (Node child : children) {
-    			Node promisingNode = getGoodNode(child);
-    			TablutBoardState playoutResult = processState(promisingNode.getState());
+    			Node promisingNode = MyTools.getGoodNode(child, user, oppo);
+    			TablutBoardState playoutResult = MyTools.processState(promisingNode.getState());
     			if (playoutResult.getWinner() == user) {
     				child.setValue(child.getValue()+1);
     			}
     		}
         }
-        return getBestValue(children).getMove();
-    }
-    
-    private Move getWinnerMove(List<Node> children){
-    	for (Node child: children) {
-    		TablutBoardState state = child.getState();
-    		if (state.getWinner() == user) {
-    			return child.getMove();
-    		}
-    	}
-    	return null;
-    }
-    
-    private Move getEscapeMove(Node parent){
-    	Move bestMove = null;
-    	Coord king = parent.getState().getKingPosition();
-    	int minDistance = Coordinates.distanceToClosestCorner(king);
-        for (TablutMove move : parent.getState().getLegalMovesForPosition(king)) {
-            int moveDistance = Coordinates.distanceToClosestCorner(move.getEndPosition());
-            if (moveDistance < minDistance) {
-            	TablutBoardState childState = (TablutBoardState) parent.getState().clone();
-                childState.processMove(move);
-            	if (captureCheck(childState)) {
-            		minDistance = moveDistance;
-                    bestMove = move;
-            	}
-            }
-        }
-        return bestMove;
-    }
-    
-    private boolean captureCheck(TablutBoardState state){
-    	int originalNumPieces = state.getNumberPlayerPieces(user);
-    	for (TablutMove move: state.getAllLegalMoves()) {
-    		TablutBoardState childState = (TablutBoardState) state.clone();
-            childState.processMove(move);
-    		int newNumPieces = childState.getNumberPlayerPieces(user);
-    		if (originalNumPieces-newNumPieces != 0) {
-    			return false;
-    		}
-    	}
-    	return true;
-    }
-    
-    private Node getBestValue(List<Node> children){
-    	Node highestScore = children.get(0);
-    	for (Node child: children) {
-    		if (child.getValue() > highestScore.getValue()){
-    			highestScore = child;
-    		}
-    	}
-    	return highestScore;
-    }
-    
-    private Node getGoodNode(Node start){
-    	start.buildChildren();
-    	int bestHeuristic = 0;
-    	Node goodNode = null;
-    	for (Node child: start.getChildren()){
-    		int heuristic = getHeuristicValue(child);
-    		if (heuristic > bestHeuristic){
-    			goodNode = child;
-    		}
-    	}
-    	return goodNode;
-    }
-    
-    private int getHeuristicValue(Node child){
-    	int value = 2000;
-    	
-    	TablutBoardState currentState = child.getState();
-    	if (currentState.gameOver()){
-    		if (currentState.getWinner() == user){
-    			return 99999;
-    		} 
-    		if (currentState.getWinner() == oppo){
-    			return 0;
-    		}
-    	}
-    	
-    	Coord kingPosition = currentState.getKingPosition(); 
-    	HashSet<Coord> userPieces = currentState.getPlayerPieceCoordinates();
-    	HashSet<Coord> oppoPieces = currentState.getOpponentPieceCoordinates();
-    	
-    	// gain points for pieces being close to king, lose points for opponent being close to king
-    	
-    	if (user == TablutBoardState.SWEDE) {
-    		for (Coord coord: currentState.getPlayerPieceCoordinates()) {
-        		value = value - Swede_SurroundWeight * coord.distance(kingPosition);
-    		}
-    		for (Coord coord: currentState.getOpponentPieceCoordinates()) {
-    			value = value + Swede_SurroundWeight * coord.distance(kingPosition);
-    		}
-    		value = value + Swede_UserPieceWeight * userPieces.size() -  Swede_OppoPieceWeight * oppoPieces.size();
-    		value -= KingEscapingWeight * Coordinates.distanceToClosestCorner(kingPosition);
-    	} else {
-    		for (Coord coord: currentState.getPlayerPieceCoordinates()) {
-        		value = value - Muscov_SurroundWeight * coord.distance(kingPosition);
-    		}
-    		for (Coord coord: currentState.getOpponentPieceCoordinates()) {
-    			value = value + Muscov_SurroundWeight * coord.distance(kingPosition);
-    		}
-    		value = value + Muscov_UserPieceWeight * userPieces.size() -  Muscov_OppoPieceWeight * oppoPieces.size();
-    		value += KingEscapingWeight * Coordinates.distanceToClosestCorner(kingPosition);
-    	}
-    	return value;
-    }
-    
-    private TablutBoardState processState(TablutBoardState state){
-    	TablutBoardState tempState = (TablutBoardState) state.clone();
-        if (tempState.gameOver()){
-            return state;
-        }
-        while (!tempState.gameOver()){
-        	Random rand = new Random();
-        	List<TablutMove> moves = tempState.getAllLegalMoves();
-        	tempState.processMove(moves.get(rand.nextInt(moves.size())));
-        }
-        return tempState;
+        return MyTools.getBestValue(children).getMove();
     }
 }
